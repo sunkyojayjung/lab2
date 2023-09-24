f3fac115533bdb95039fff93779f9544b204bb6d
sunkyojayjung <sunkyo98@hotmail.com>
2023-09-24 16:49:42 -0500

    step 18 clear

--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -1,11 +1,20 @@
 package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickFunction(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
